e32eb45674b7ca3cf5353809a21aa04f7a5917e7
zacktan <zacktanzx1231@gmail.com>
2025-11-28 16:38:34 +0800

    Load data into handsontable  as json

--- risk/urls.py
+++ risk/urls.py
@@ -1,18 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name="polls"
-
-
-
 urlpatterns = [
     path('', views.index, name='risk_home'),
+    path('daily_risk_data/', views.daily_risk_data, name='daily_risk_data')
 ]
-
-# urlpatterns = [
-#     path("", views.IndexView.as_view(), name="index"),
-#     # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-#     # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-#     # path("<int:question_id>/vote/", views.vote, name="vote")
-# ]
 
--- risk/views.py
+++ risk/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import DailyRisk
+
 
 def index(request):
     return render(request, "risk/pm_risk_table.html")
+
+def daily_risk_data(request):
+    qs = DailyRisk.objects.all().values(
+        'book','risk','target','stop','worst_case_bp','worst_case_k','comment'
+    ).order_by('-date')
+    return JsonResponse(list(qs), safe=False)
