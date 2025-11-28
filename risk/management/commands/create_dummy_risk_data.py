b314f33696b460403bbe0dcf1949e1881709b688
zacktan <zacktanzx1231@gmail.com>
2025-11-28 17:11:15 +0800

    Add historical dummy data

--- risk/management/commands/create_dummy_risk_data.py
+++ risk/management/commands/create_dummy_risk_data.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from risk.models import DailyRisk
-from datetime import date
+from datetime import date, timedelta
 
 class Command(BaseCommand):
     help = "Create dummy DailyRisk records for testing"
@@ -13,7 +13,7 @@ class Command(BaseCommand):
             defaults={'email': 'zt@gmail.com'}
         )
         
-        dummy_data = [
+        dummy_data_today = [
             {
                 "book": "Emerging Mkts",
                 "risk": 12500.00,
@@ -42,8 +42,41 @@ class Command(BaseCommand):
                 "comment": "Tight spreads; minimal stress expected"
             }
         ]
+        
+
+        dummy_data_past = [
+            {
+                "book": "B1",
+                "risk": 10000.00,
+                "target": 5.00,
+                "stop": 10.00,
+                "worst_case_bp": 10.00,
+                "worst_case_k": 1875.00,
+                "comment": "High vol in EM FX"
+            },
+            {
+                "book": "B2",
+                "risk": 8000.00,
+                "target": 5.50,
+                "stop": 7.50,
+                "worst_case_bp": 10.00,
+                "worst_case_k": 820.00,
+                "comment": "Some comments"
+            },
+            {
+                "book": "B3",
+                "risk": 5000.00,
+                "target": 3.00,
+                "stop": 5.00,
+                "worst_case_bp": 6.00,
+                "worst_case_k": 300.00,
+                "comment": "Tight spreads; minimal stress expected"
+            }
+        ]
+        
+        
         count = 0 
-        for data in dummy_data : 
+        for data in dummy_data_today : 
             obj, created = DailyRisk.objects.get_or_create(
                 pm=user,
                 date=date.today(),
@@ -53,6 +86,17 @@ class Command(BaseCommand):
             if created : 
                 count +=1
 
+        for data in dummy_data_past : 
+            obj, created = DailyRisk.objects.get_or_create(
+                pm=user,
+                date=date.today()-timedelta(days=1),
+                book=data['book'],
+                defaults=data
+            )
+            if created : 
+                count +=1
+        
+        
         self.stdout.write(
             self.style.SUCCESS(f'Successfully created {count} dummy risk records')
         )
